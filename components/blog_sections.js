4e5b4a12c71f340856927b0d2f60ba18563ae597
Austin <austincolepage@hotmail.com>
2022-07-16 23:42:32 -0400

    updates to projects

--- components/blog_sections.js
+++ components/blog_sections.js
@@ -1,25 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Post from "../components/post";
+import CenterScreen from "../utils/CenterScreen";
 
 const Blog = () => {
     return (
-        <div className="">
+        <CenterScreen>
             <div>
-                <h1>Projects</h1>
-                <p>dfghjfghdejkhceklj;jkfevcjefjklfejklfvjlk;</p>
-            </div>
+                <div className="p-4">
+                    <h1 className="text-2xl font-bold md:text-4xl">
+                        Research Projects
+                    </h1>
+                    <p className="text-gray-500">
+                        Check out some of the projects from our lab.
+                    </p>
+                    <hr className="w-[90%] my-6"></hr>
+                </div>
 
-            <hr></hr>
-
-            <div className="grid grid-cols-2 gap-4">
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
+                <div className="grid gap-4 grid-cols-1 lg:grid-cols-2">
+                    <Post />
+                    <Post />
+                    <Post />
+                    <Post />
+                    <Post />
+                    <Post />
+                    <Post />
+                    <Post />
+                    <Post />
+                    <Post />
+                    <Post />
+                </div>
             </div>
-        </div>
+        </CenterScreen>
     );
 };
 
--- components/post.js
+++ components/post.js
@@ -1,14 +1,24 @@
 import React from "react";
 
+const truncate = (str) => {
+    return str.length > 400 ? str.substring(0, 400) + "..." : str;
+};
+
 const Post = ({ date, heading, content, media }) => {
     return (
-        <div className="flex flex-col flex-wrap h-[300px] bg-gray-600">
-            <p className="grow-[2] bg-white m-1">Date</p>
-            <h1 className="grow-[2] bg-white m-1">Heading</h1>
-            <p className="grow-[10] bg-white m-1">
-                ContentContentContentContentCont
+        <div className="p-4 flex flex-col h-[200px]">
+            <p className="grow-[1] text-gray-500 text-xs">March 16, 2020</p>
+            <h1 className="grow-[1] text-black font-bold md:text-2xl">
+                Boost your conversion rate
+            </h1>
+            <p className="grow-[3] text-gray-500 text-sm mb-2 md:text-[16px]">
+                {truncate(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta lacus mi, ut volutpat mauris pellentesque quis. Nulla facilisi. Donec hendrerit nulla ipsum, eget suscipit felis convallis eu. Nulla hendrerit quis odio sit amet convallis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta lacus mi, ut volutpat mauris pellentesque quis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta lacus mi, ut volutpat mauris pellentesque quis. Nulla facilisi. Donec hendrerit nulla ipsum, eget suscipit felis convallis eu. Nulla hendrerit quis odio sit amet convallis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porta lacus mi, ut volutpat mauris pellentesque quis"
+                )}
             </p>
-            <p className="bg-white m-1">READ FULL STORY</p>
+            <p className="grow-[1] text-indigo-600 text-sm font-bold">
+                Read More
+            </p>
         </div>
     );
 };
