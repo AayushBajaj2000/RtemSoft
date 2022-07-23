889b3c2da48ac49d089844a46a4eaf9ed9b2c3e5
Aayush <aayubajaj2000@gmail.com>
2022-07-23 03:27:24 -0400

    added simple page transitions

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,30 @@ import "../styles/globals.css";
 import React from "react";
 import Layout from "../components/Layout";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { motion } from "framer-motion";
 
 const queryClient = new QueryClient();
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
     <QueryClientProvider client={queryClient}>
       <Layout>
-        <Component {...pageProps} />
+        <motion.div
+          key={router.route}
+          initial="pageInitial"
+          animate="pageAnimate"
+          variants={{
+            pageInitial: {
+              opacity: 0,
+            },
+            pageAnimate: {
+              opacity: 1,
+            },
+          }}
+          transition={{ delay: 0.2, type: "tween" }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
       </Layout>
     </QueryClientProvider>
   );
