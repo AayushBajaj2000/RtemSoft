c4fec1d77c13efad89bd66de309cac5fc2971dc6
Austin <austincolepage@hotmail.com>
2022-07-27 19:23:13 -0400

    pop-up

--- components/post.js
+++ components/post.js
@@ -1,22 +1,120 @@
 import React from "react";
+import { Fragment, useRef, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { ExclamationIcon } from "@heroicons/react/outline";
 
 const truncate = (str) => {
-  return str.length > 400 ? str.substring(0, 400) + "..." : str;
+    return str.length > 400 ? str.substring(0, 400) + "..." : str;
 };
 
 const Post = ({ date, heading, content, media }) => {
-  return (
-    <div className="bg-white shadow-lg rounded-lg p-4 flex flex-col h-[300px]">
-      <p className="h-[20px] text-gray-500 text-xs">{date}</p>
-      <h1 className="h-[80px] text-black font-bold md:text-2xl">{heading}</h1>
-      <p className="h-[120px] text-gray-500 text-sm mb-2 md:text-[16px]">
-        {truncate(content)}
-      </p>
-      <p className="hover:cursor-pointer hover:text-indigo-400 h-[20px] text-indigo-600 text-sm font-bold">
-        Read More
-      </p>
-    </div>
-  );
+    const [open, setOpen] = useState(false);
+    const cancelButtonRef = useRef(null);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    return (
+        <>
+            <div className="bg-white shadow-lg rounded-lg p-4 flex flex-col h-[300px]">
+                <p className="h-[20px] text-gray-500 text-xs">{date}</p>
+                <h1 className="h-[80px] text-black font-bold md:text-2xl">
+                    {heading}
+                </h1>
+                <p className="h-[120px] text-gray-500 text-sm mb-2 md:text-[16px]">
+                    {truncate(content)}
+                </p>
+                <p
+                    onClick={handleOpen}
+                    className="hover:cursor-pointer hover:text-indigo-400 h-[20px] text-indigo-600 text-sm font-bold"
+                >
+                    Read More
+                </p>
+            </div>
+
+            <Transition.Root show={open} as={Fragment}>
+                <Dialog
+                    as="div"
+                    className="relative z-10"
+                    initialFocus={cancelButtonRef}
+                    onClose={setOpen}
+                >
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                    </Transition.Child>
+
+                    <div className="fixed z-10 inset-0 overflow-y-auto">
+                        <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            >
+                                <Dialog.Panel className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
+                                    <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                                        <div className="sm:flex sm:items-start">
+                                            <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                                <ExclamationIcon
+                                                    className="h-6 w-6 text-emerald-500"
+                                                    aria-hidden="true"
+                                                />
+                                            </div>
+                                            <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                                <Dialog.Title
+                                                    as="h3"
+                                                    className="text-lg leading-6 font-medium text-gray-900"
+                                                >
+                                                    Deactivate account
+                                                </Dialog.Title>
+                                                <div className="mt-2">
+                                                    <p className="text-sm text-gray-500">
+                                                        Are you sure you want to
+                                                        deactivate your account?
+                                                        All of your data will be
+                                                        permanently removed.
+                                                        This action cannot be
+                                                        undone.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                                        <button
+                                            type="button"
+                                            className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-emerald-500 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                                            onClick={() => setOpen(false)}
+                                        >
+                                            Deactivate
+                                        </button>
+                                        <button
+                                            type="button"
+                                            className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                                            onClick={() => setOpen(false)}
+                                            ref={cancelButtonRef}
+                                        >
+                                            Cancel
+                                        </button>
+                                    </div>
+                                </Dialog.Panel>
+                            </Transition.Child>
+                        </div>
+                    </div>
+                </Dialog>
+            </Transition.Root>
+        </>
+    );
 };
 
 export default Post;
